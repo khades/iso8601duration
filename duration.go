db3984c4aea411822176fe158522635351326b33
khades <khades@khades.org>
2018-01-16 20:48:15 +0700

    Direct parsing to time.Duration

--- duration.go
+++ duration.go
@@ -2,7 +2,6 @@
 package duration
 
 import (
-	"bytes"
 	"errors"
 	"fmt"
 	"regexp"
@@ -24,16 +23,8 @@ var (
 	week = regexp.MustCompile(`P((?P<week>\d+)W)`)
 )
 
-type Duration struct {
-	Years   int
-	Weeks   int
-	Days    int
-	Hours   int
-	Minutes int
-	Seconds int
-}
-
-func FromString(dur string) (*Duration, error) {
+func ParseString(dur string) (*time.Duration, error) {
+	var duration time.Duration
 	var (
 		match []string
 		re    *regexp.Regexp
@@ -49,8 +40,6 @@ func FromString(dur string) (*Duration, error) {
 		return nil, ErrBadFormat
 	}
 
-	d := &Duration{}
-
 	for i, name := range re.SubexpNames() {
 		part := match[i]
 		if i == 0 || name == "" || part == "" {
@@ -63,59 +52,22 @@ func FromString(dur string) (*Duration, error) {
 		}
 		switch name {
 		case "year":
-			d.Years = val
+			duration = duration + 24*365*time.Duration(val)*time.Hour
 		case "month":
 			return nil, ErrNoMonth
 		case "week":
-			d.Weeks = val
+			duration = duration + 24*7*time.Duration(val)*time.Hour
 		case "day":
-			d.Days = val
+			duration = duration + 24*time.Duration(val)*time.Hour
 		case "hour":
-			d.Hours = val
+			duration = duration + time.Duration(val)*time.Hour
 		case "minute":
-			d.Minutes = val
+			duration = duration + time.Duration(val)*time.Minute
 		case "second":
-			d.Seconds = val
+			duration = duration + time.Duration(val)*time.Second
 		default:
 			return nil, errors.New(fmt.Sprintf("unknown field %s", name))
 		}
 	}
-
-	return d, nil
-}
-
-// String prints out the value passed in. It's not strictly according to the
-// ISO spec, but it's pretty close. In particular, to completely conform it
-// would need to round up to the next largest unit. 61 seconds to 1 minute 1
-// second, for example. It would also need to disallow weeks mingling with
-// other units.
-func (d *Duration) String() string {
-	var s bytes.Buffer
-
-	err := tmpl.Execute(&s, d)
-	if err != nil {
-		panic(err)
-	}
-
-	return s.String()
-}
-
-func (d *Duration) HasTimePart() bool {
-	return d.Hours != 0 || d.Minutes != 0 || d.Seconds != 0
-}
-
-func (d *Duration) ToDuration() time.Duration {
-	day := time.Hour * 24
-	year := day * 365
-
-	tot := time.Duration(0)
-
-	tot += year * time.Duration(d.Years)
-	tot += day * 7 * time.Duration(d.Weeks)
-	tot += day * time.Duration(d.Days)
-	tot += time.Hour * time.Duration(d.Hours)
-	tot += time.Minute * time.Duration(d.Minutes)
-	tot += time.Second * time.Duration(d.Seconds)
-
-	return tot
+	return &duration, nil
 }
--- duration_test.go
+++ duration_test.go
@@ -1,81 +1,44 @@
-package duration_test
+package duration
 
 import (
+	"log"
 	"testing"
 	"time"
-
-	duration "github.com/channelmeter/iso8601duration"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestFromString(t *testing.T) {
-	t.Parallel()
 
 	// test with bad format
-	_, err := duration.FromString("asdf")
-	assert.Equal(t, err, duration.ErrBadFormat)
-
+	_, err := ParseString("asdf")
+	if err != ErrBadFormat {
+		t.Error("Should give exception on random string")
+	}
 	// test with month
-	_, err = duration.FromString("P1M")
-	assert.Equal(t, err, duration.ErrNoMonth)
+	_, err = ParseString("P1M")
 
+	if err != ErrNoMonth  {
+		t.Error("Should give exception when there's month")
+	}
 	// test with good full string
-	dur, err := duration.FromString("P1Y2DT3H4M5S")
-	assert.Nil(t, err)
-	assert.Equal(t, 1, dur.Years)
-	assert.Equal(t, 2, dur.Days)
-	assert.Equal(t, 3, dur.Hours)
-	assert.Equal(t, 4, dur.Minutes)
-	assert.Equal(t, 5, dur.Seconds)
+	dur, err := ParseString("P1Y2DT3H4M5S")
+	if err != nil {
+		t.Error("Full string should parse properly")
+	}
+	fullDuration := 24*365*time.Hour + 2*24*time.Hour + 3*time.Hour + 4*time.Minute + 5*time.Second
+	log.Println(dur)
+	if fullDuration.Nanoseconds() != dur.Nanoseconds() {
+		t.Error("Full string parsing is inaccurate")
+
+	}
 
 	// test with good week string
-	dur, err = duration.FromString("P1W")
-	assert.Nil(t, err)
-	assert.Equal(t, 1, dur.Weeks)
-}
+	dur, err = ParseString("P1W")
+	if err != nil {
+		t.Error("Week string should parse properly")
+	}
+	smallDuration := 7 * 24 * time.Hour
+	if smallDuration.Nanoseconds() != dur.Nanoseconds() {
+		t.Error("Week string parsing is inaccurate")
 
-func TestString(t *testing.T) {
-	t.Parallel()
-
-	// test empty
-	d := duration.Duration{}
-	assert.Equal(t, d.String(), "P")
-
-	// test only larger-than-day
-	d = duration.Duration{Years: 1, Days: 2}
-	assert.Equal(t, d.String(), "P1Y2D")
-
-	// test only smaller-than-day
-	d = duration.Duration{Hours: 1, Minutes: 2, Seconds: 3}
-	assert.Equal(t, d.String(), "PT1H2M3S")
-
-	// test full format
-	d = duration.Duration{Years: 1, Days: 2, Hours: 3, Minutes: 4, Seconds: 5}
-	assert.Equal(t, d.String(), "P1Y2DT3H4M5S")
-
-	// test week format
-	d = duration.Duration{Weeks: 1}
-	assert.Equal(t, d.String(), "P1W")
-}
-
-func TestToDuration(t *testing.T) {
-	t.Parallel()
-
-	d := duration.Duration{Years: 1}
-	assert.Equal(t, d.ToDuration(), time.Hour*24*365)
-
-	d = duration.Duration{Weeks: 1}
-	assert.Equal(t, d.ToDuration(), time.Hour*24*7)
-
-	d = duration.Duration{Days: 1}
-	assert.Equal(t, d.ToDuration(), time.Hour*24)
-
-	d = duration.Duration{Hours: 1}
-	assert.Equal(t, d.ToDuration(), time.Hour)
-
-	d = duration.Duration{Minutes: 1}
-	assert.Equal(t, d.ToDuration(), time.Minute)
-
-	d = duration.Duration{Seconds: 1}
-	assert.Equal(t, d.ToDuration(), time.Second)
+	}
 }
